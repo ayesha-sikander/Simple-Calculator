9e6813175ad1d0f70d929ad23ca0af146416184a
ayesha-sikander <aashii0305@gmail.com>
2024-03-23 00:04:50 +0500

    first commit

---
+++ class.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+//Printing a Welcome Message
+console.log("\n\tWelcome To \'CodeWithAyesha\' - CLI Simple Calculator\n");
+
+// Asking Questions from Users through Inquirer
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    {message: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+        message: "Select One Operator to perform Operations",
+        type: "list", 
+        name: "operator",
+        choices: ["Addition","Subtraction","Multipication","Division"],
+    },
+]);
+
+//Conditional statements If-Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtraction"){
+     console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multipication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
